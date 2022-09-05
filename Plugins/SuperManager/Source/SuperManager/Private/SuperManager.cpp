c0e267e8b0bca3336c7529609a3661233722d8ec
Vince Zheng <371096148@qq.com>
2022-09-05 11:32:29 +0800

    ModulesDone

---
+++ Plugins/SuperManager/Source/SuperManager/Private/SuperManager.cpp
@@ -0,0 +1,20 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "SuperManager.h"
+
+#define LOCTEXT_NAMESPACE "FSuperManagerModule"
+
+void FSuperManagerModule::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FSuperManagerModule::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FSuperManagerModule, SuperManager)
