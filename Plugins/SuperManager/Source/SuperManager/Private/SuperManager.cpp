b7134426c3e196f790f3d3bda09d1c0f16cbe072
Vince Zheng <371096148@qq.com>
2022-09-10 11:26:35 +0800

    Delete Empty Folders Done

--- Plugins/SuperManager/Source/SuperManager/Private/SuperManager.cpp
+++ Plugins/SuperManager/Source/SuperManager/Private/SuperManager.cpp
@@ -135,8 +135,58 @@ void FSuperManagerModule::OnDeleteUnsuedAssetButtonClicked()
 }
 
 void FSuperManagerModule::OnDeleteEmptyFoldersButtonClicked()
-{
-	DebugHeader::Print(TEXT("Working"),FColor::Green);
+{	
+	FixUpRedirectors();
+
+	TArray<FString> FolderPathsArray = UEditorAssetLibrary::ListAssets(FolderPathsSelected[0],true,true);
+	uint32 Counter = 0;
+
+	FString EmptyFolderPathsNames;
+	TArray<FString> EmptyFoldersPathsArray;
+
+	for(const FString& FolderPath:FolderPathsArray)
+	{
+		if(FolderPath.Contains(TEXT("Developers"))||
+		FolderPath.Contains(TEXT("Collections")) ||
+		FolderPath.Contains(TEXT("__ExternalActors__")) ||
+		FolderPath.Contains(TEXT("__ExternalObjects__")))
+		{
+			continue;
+		}
+
+		if(!UEditorAssetLibrary::DoesDirectoryExist(FolderPath)) continue;
+
+		if(!UEditorAssetLibrary::DoesDirectoryHaveAssets(FolderPath))
+		{
+			EmptyFolderPathsNames.Append(FolderPath);
+			EmptyFolderPathsNames.Append(TEXT("\n"));
+
+			EmptyFoldersPathsArray.Add(FolderPath);
+		}		
+	}
+
+	if(EmptyFoldersPathsArray.Num()==0)
+	{
+		DebugHeader::ShowMsgDialog(EAppMsgType::Ok,TEXT("No empty folder found under selected folder"),false);
+		return;
+	}
+
+	EAppReturnType::Type ConfirmResult = DebugHeader::ShowMsgDialog(EAppMsgType::OkCancel,
+	TEXT("Empty folders found in:\n") + EmptyFolderPathsNames +TEXT("\nWould you like to delete all?"),false);
+
+	if(ConfirmResult==EAppReturnType::Cancel) return;
+	
+	for(const FString& EmptyFolderPath:EmptyFoldersPathsArray)
+	{
+		UEditorAssetLibrary::DeleteDirectory(EmptyFolderPath)?
+		++Counter : DebugHeader::Print(TEXT("Failed to delete " + EmptyFolderPath),FColor::Red);
+	}
+
+	if(Counter>0)
+	{
+		DebugHeader::ShowNotifyInfo(TEXT("Successfully deleted ") + FString::FromInt(Counter) + TEXT("folders"));
+	}
+
 }
 
 void FSuperManagerModule::FixUpRedirectors()
