dfa335137daf1b94ac0902a772bf43e5988b79d5
Vince Zheng <371096148@qq.com>
2022-09-19 10:20:23 +0800

    SupportedTextureNamesDone

--- Plugins/SuperManager/Source/SuperManager/Public/AssetActions/QuickMaterialCreationWidget.h
+++ Plugins/SuperManager/Source/SuperManager/Public/AssetActions/QuickMaterialCreationWidget.h
@@ -29,6 +29,45 @@ public:
 
 #pragma endregion
 
+#pragma region SupportedTextureNames
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Supported Texture Names")
+	TArray<FString> BaseColorArray = {
+		TEXT("_BaseColor"),
+		TEXT("_Albedo"),
+		TEXT("_Diffuse"),
+		TEXT("_diff")
+	};
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Supported Texture Names")
+	TArray<FString> MetallicArray = {
+		TEXT("_Metallic"),
+		TEXT("_metal")
+	};
+
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Supported Texture Names")
+	TArray<FString> RoughnessArray = {
+		TEXT("_Roughness"),
+		TEXT("_RoughnessMap"),
+		TEXT("_rough")
+	};
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Supported Texture Names")
+	TArray<FString> NormalArray = {
+		TEXT("_Normal"),
+		TEXT("_NormalMap"),
+		TEXT("_nor")
+	};
+	              
+	UPROPERTY(EditAnywhere,BlueprintReadWrite,Category = "Supported Texture Names")
+	TArray<FString> AmbientOcclusionArray = {
+		TEXT("_AmbientOcclusion"),
+		TEXT("_AmbientOcclusionMap"),
+		TEXT("_AO")
+	};
+
+#pragma endregion
+
 private:
 
 #pragma region QuickMaterialCreation
