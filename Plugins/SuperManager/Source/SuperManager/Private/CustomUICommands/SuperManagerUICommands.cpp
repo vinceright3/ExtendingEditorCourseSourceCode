acc7190fd92f1a30011dc980937e99c34e3cc9a0
Vince Zheng <371096148@qq.com>
2022-09-24 12:22:31 +0800

    PreparingForHotKeysDone

---
+++ Plugins/SuperManager/Source/SuperManager/Private/CustomUICommands/SuperManagerUICommands.cpp
@@ -0,0 +1,8 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomUICommands/SuperManagerUICommands.h"
+
+void FSuperManagerUICommands::RegisterCommands()
+{
+}
---
+++ Plugins/SuperManager/Source/SuperManager/Public/CustomUICommands/SuperManagerUICommands.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "Framework/Commands/Commands.h"
+
+
+class FSuperManagerUICommands : public TCommands<FSuperManagerUICommands>
+{
+public:
+	FSuperManagerUICommands() : TCommands<FSuperManagerUICommands>(
+	TEXT("SuperManager"),
+	FText::FromString(TEXT("Super Manager UI Commands")),
+	NAME_None,
+	TEXT("SuperManager")
+	) {}
+
+	virtual void RegisterCommands() override;
+};
